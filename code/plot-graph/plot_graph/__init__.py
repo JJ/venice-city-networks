01a762ecb87a0eb931640e89f5257c3f4bf27066
JJ Merelo <jjmerelo@gmail.com>
2024-06-30 09:55:37 +0200

    #1 attempt with Python - failed

--- code/plot-graph/plot_graph/__init__.py
+++ code/plot-graph/plot_graph/__init__.py
@@ -1,11 +1,11 @@
 import igraph as ig
 
-def main():
+def __main__():
     # Load the graph from the graphml file
     graph = ig.Graph.Read_GraphML("../data/venice.graphml")
 
     # Plot the graph
-    ig.plot(graph)
+    ig.plot(graph, target="../data/venice.pdf")
 
 if __name__ == "__main__":
     main()
