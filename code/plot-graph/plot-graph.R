a47653465a5b54bc539a21a286b79ee2827ee03c
JJ Merelo <jjmerelo@gmail.com>
2024-06-30 12:06:20 +0200

    closes #3 changed width type
    Closed #1 working example

--- code/plot-graph/plot-graph.R
+++ code/plot-graph/plot-graph.R
@@ -1,5 +1,10 @@
 library(igraph)
 
-venice.graph <- read_graph("data/venice-redone.graphml",format="graphml")
-plot(venice.graph)
-#
+venice.graph <- as.undirected( read_graph("data/venice.graphml",format="graphml") )
+
+# graph.betw <- betweenness(venice.graph)
+E(venice.graph)$width <- round(log(edge_betweenness(venice.graph))/2)
+E(venice.graph)[ E(venice.graph)$width == -Inf]$width = 1
+
+plot(venice.graph,vertex.label=NA,vertex.shape="none",edge.color=E(venice.graph)$width)
+
