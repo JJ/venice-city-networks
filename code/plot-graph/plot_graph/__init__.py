6fbacefd9bf7a533dc001ec3b8e84b1abff58b4d
JJ Merelo <jjmerelo@gmail.com>
2024-06-30 10:33:19 +0200

    Trying again

--- code/plot-graph/plot_graph/__init__.py
+++ code/plot-graph/plot_graph/__init__.py
@@ -1,4 +1,7 @@
 import igraph as ig
+import networkx as nx
+import time
+import matplotlib.pyplot as plt
 
 def __main__():
     # Load the graph from the graphml file
@@ -6,6 +9,15 @@ def __main__():
 
     # Plot the graph
     ig.plot(graph, target="../data/venice.pdf")
+
+def v2():
+    # load the graph using networkx
+    G = nx.read_graphml("../data/venice.graphml")
+    # plot graph and wait for exit
+    nx.draw(G)
+    # wait 30 seconds to exit
+    time.sleep(30)    
+
 
 if __name__ == "__main__":
     main()
