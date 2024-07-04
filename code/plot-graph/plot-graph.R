6d50486c0c464c294e5dd0fa9ab5537eea4da722
JJ Merelo <jjmerelo@gmail.com>
2024-07-04 20:19:37 +0200

    Make attributes

--- code/plot-graph/plot-graph.R
+++ code/plot-graph/plot-graph.R
@@ -1,6 +1,12 @@
 library(igraph)
+library(xml2)
+library(dplyr)
 
 venice.graph <- as.undirected( read_graph("code/data/venice.graphml",format="graphml") )
+venice.graph.xml <- read_xml("code/data/venice.graphml")
+edge.type <- xml_text(xml_find_all(venice.graph.xml,".//d1:data[@key='d3']"))
+
+edge.type %>% table
 
 # graph.betw <- betweenness(venice.graph)
 E(venice.graph)$width <- round(log(edge_betweenness(venice.graph))/2)
