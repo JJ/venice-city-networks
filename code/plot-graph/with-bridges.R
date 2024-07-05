c3a26b9d3be0c6788f731bf4be17933bbb3dde99
JJ Merelo <jjmerelo@gmail.com>
2024-07-05 17:33:23 +0200

    closes #6 not needed #4

---
+++ code/plot-graph/with-bridges.R
@@ -0,0 +1,42 @@
+library(igraph)
+library(xml2)
+library(dplyr)
+
+venice.graph <- read_graph("code/data/venice.graphml",format="graphml")
+venice.graph.xml <- read_xml("code/data/venice.graphml")
+edges <- xml_text(xml_find_all(venice.graph.xml,".//d1:edge"))
+
+edge.lty <- NULL
+
+
+for (i in 1:length(edges)){
+  print(i)
+  print(xml_find_all(edges[i], ".//d1:data[@key='d1']"))
+  if xml_find_all(edges[i], ".//d1:data[@key='d1']") != NULL {
+    E(venice.graph)[i]$bridge <- TRUE
+    edge.lty <- c(edge.lty,2)
+  } else
+    edge.lty <- c(edge.lty,1)
+}
+
+E(venice.graph)$edge.lty <- edge.lty
+
+venice.graph.undirected <- as.undirected(venice.graph)
+
+for ( edge in E(venice.graph.undirected) ){
+  source <- ends(venice.graph.undirected, edge)[1]
+  target <- ends(venice.graph.undirected, edge)[2]
+
+  # select edge in venice.graph with source source and target target
+  directed.edge <- get.edge.ids(venice.graph, c(source, target), directed = TRUE)
+  E(venice.graph.undirected)[edge]$edge.lty <- E(venice.graph)[directed.edge]$edge.lty
+}
+
+# graph.betw <- betweenness(venice.graph)
+E(venice.graph.undirected)$width <- round(log(edge_betweenness(venice.graph.undirected))/2)
+E(venice.graph.undirected)[ E(venice.graph.undirected)$width == -Inf]$width = 1
+
+plot(venice.graph.undirected,vertex.label=NA,vertex.shape="none",edge.color=E(venice.graph.undirected)$width, edge.lty = E(venice.graph)$edge.lty)
+
+saveRDS(venice.graph.undirected, file="code/data/venice.graph.undirected.rds")
+save(venice.graph.undirected, file="code/data/venice.graph.undirected.Rdata")
