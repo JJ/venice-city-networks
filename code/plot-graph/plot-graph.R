5bbafbeb7cf3227fd80947cc12601e8bc4fdd762
JJ Merelo <jjmerelo@gmail.com>
2024-07-05 13:36:24 +0200

    Saving to data to avoid computation

--- code/plot-graph/plot-graph.R
+++ code/plot-graph/plot-graph.R
@@ -2,31 +2,43 @@ library(igraph)
 library(xml2)
 library(dplyr)
 
-venice.graph <- as.undirected( read_graph("code/data/venice.graphml",format="graphml") )
+venice.graph <- read_graph("code/data/venice.graphml",format="graphml")
 venice.graph.xml <- read_xml("code/data/venice.graphml")
-edges.original <- xml_find_all(venice.graph.xml,".//d1:edge")
+edge.type <- xml_text(xml_find_all(venice.graph.xml,".//d1:data[@key='d3']"))
 
-# process edges.original extracting for every one attributes source and target, and aggregating data nodes with key d2 and d3
+E(venice.graph)$type <- edge.type
 
-
-# Initialize empty vector
 edge.lty <- NULL
 
-# assign type 1 if pedestrian, 2 if footway, 3 if ['steps', 'pedestrian'], 4 if ['steps', 'pedestrian', 'footway'] , 5 if service, 6 for anything else
+
 for (i in 1:length(edge.type)){
-  if (edge.type[i] == "pedestrian") edge.lty <- c(edge.lty,1)
-  else if (edge.type[i] == "footway") edge.lty <- c(edge.lty,2)
-  else if (edge.type[i] == "steps") edge.lty <- c(edge.lty,3)
-  else if (edge.type[i] == "['steps', 'pedestrian']") edge.lty <- c(edge.lty,4)
-  else if (edge.type[i] == "['steps', 'pedestrian', 'footway']") edge.lty <- c(edge.lty,5)
-  else if (edge.type[i] == "service") edge.lty <- c(edge.lty,6)
+  if (grepl("steps",edge.type[i]) == TRUE ) edge.lty <- c(edge.lty,1)
+  else if ( grepl("pedestrian",edge.type[i]) == TRUE ) edge.lty <- c(edge.lty,2)
+  else if ( grepl("footway",edge.type[i]) == TRUE ) edge.lty <- c(edge.lty,3)
+  else if ( grepl("service",edge.type[i]) == TRUE ) edge.lty <- c(edge.lty,4)
+  else if ( grepl("unclassified",edge.type[i]) == TRUE ) edge.lty <- c(edge.lty,5)
+  else if ( grepl("primary",edge.type[i]) == TRUE ) edge.lty <- c(edge.lty,6)
   else edge.lty <- c(edge.lty,7)
 }
 
+E(venice.graph)$edge.lty <- edge.lty
+
+venice.graph.undirected <- as.undirected(venice.graph)
+
+for ( edge in E(venice.graph.undirected) ){
+  source <- ends(venice.graph.undirected, edge)[1]
+  target <- ends(venice.graph.undirected, edge)[2]
+
+  # select edge in venice.graph with source source and target target
+  directed.edge <- get.edge.ids(venice.graph, c(source, target), directed = TRUE)
+  E(venice.graph.undirected)[edge]$edge.lty <- E(venice.graph)[directed.edge]$edge.lty
+}
+
 # graph.betw <- betweenness(venice.graph)
-E(venice.graph)$width <- round(log(edge_betweenness(venice.graph))/2)
-E(venice.graph)[ E(venice.graph)$width == -Inf]$width = 1
-E(venice.graph)$lty <- edge.lty
+E(venice.graph.undirected)$width <- round(log(edge_betweenness(venice.graph.undirected))/2)
+E(venice.graph.undirected)[ E(venice.graph.undirected)$width == -Inf]$width = 1
 
-plot(venice.graph,vertex.label=NA,vertex.shape="none",edge.color=E(venice.graph)$width, edge.lty = E(venice.graph)$lty)
+plot(venice.graph.undirected,vertex.label=NA,vertex.shape="none",edge.color=E(venice.graph.undirected)$width, edge.lty = E(venice.graph)$edge.lty)
 
+saveRDS(venice.graph.undirected, file="code/data/venice.graph.undirected.rds")
+save(venice.graph.undirected, file="code/data/venice.graph.undirected.Rdata")
