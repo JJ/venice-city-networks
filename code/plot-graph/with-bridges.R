31b51487590d5392fd28bb0312b2b5bd9e15f03d
JJ Merelo <jjmerelo@gmail.com>
2024-07-08 12:24:53 +0200

    :recycle:

--- code/plot-graph/with-bridges.R
+++ code/plot-graph/with-bridges.R
@@ -4,15 +4,12 @@ library(dplyr)
 
 venice.graph <- read_graph("code/data/venice.graphml",format="graphml")
 venice.graph.xml <- read_xml("code/data/venice.graphml")
-edges <- xml_text(xml_find_all(venice.graph.xml,".//d1:edge"))
+edges <- xml_find_all(venice.graph.xml,".//d1:edge")
 
 edge.lty <- NULL
 
-
 for (i in 1:length(edges)){
-  print(i)
-  print(xml_find_all(edges[i], ".//d1:data[@key='d1']"))
-  if xml_find_all(edges[i], ".//d1:data[@key='d1']") != NULL {
+  if ( grepl('\"d1\"', edges[i]) ) {
     E(venice.graph)[i]$bridge <- TRUE
     edge.lty <- c(edge.lty,2)
   } else
@@ -27,7 +24,6 @@ for ( edge in E(venice.graph.undirected) ){
   source <- ends(venice.graph.undirected, edge)[1]
   target <- ends(venice.graph.undirected, edge)[2]
 
-  # select edge in venice.graph with source source and target target
   directed.edge <- get.edge.ids(venice.graph, c(source, target), directed = TRUE)
   E(venice.graph.undirected)[edge]$edge.lty <- E(venice.graph)[directed.edge]$edge.lty
 }
@@ -36,7 +32,8 @@ for ( edge in E(venice.graph.undirected) ){
 E(venice.graph.undirected)$width <- round(log(edge_betweenness(venice.graph.undirected))/2)
 E(venice.graph.undirected)[ E(venice.graph.undirected)$width == -Inf]$width = 1
 
-plot(venice.graph.undirected,vertex.label=NA,vertex.shape="none",edge.color=E(venice.graph.undirected)$width, edge.lty = E(venice.graph)$edge.lty)
+plot(venice.graph.undirected,vertex.label=NA,vertex.shape="none",edge.color=E(venice.graph.undirected)$edge.lty, edge.lty = E(venice.graph)$edge.lty)
 
-saveRDS(venice.graph.undirected, file="code/data/venice.graph.undirected.rds")
-save(venice.graph.undirected, file="code/data/venice.graph.undirected.Rdata")
+venice.graph.bridges <- venice.graph.undirected
+saveRDS(venice.graph.bridges, file="code/data/venice.graph.bridges.rds")
+save(venice.graph.bridges, file="code/data/venice.graph.bridges.Rdata")
